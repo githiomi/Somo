71331dbbb9953a3bf9232e74d8900dc3bf938f14
githiomi <danielgithiomi@gmail.com>
2021-05-08 00:58:43 +0400

    Created secondary activity and inserted the frame layout

---
+++ app/src/main/java/com/githiomi/somo/UI/SecondaryActivity.java
@@ -0,0 +1,16 @@
+package com.githiomi.somo.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.githiomi.somo.R;
+
+public class SecondaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondary);
+    }
+}
